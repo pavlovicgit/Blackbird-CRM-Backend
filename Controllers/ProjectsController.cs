a10d7426906af0d7f0a0846bdc268e7f340b0628
zz-4 <marko.r.pavlovic@protonmail.com>
2024-10-04 00:22:08 +0200

    Minor change to get clients endpoint

--- Controllers/ProjectsController.cs
+++ Controllers/ProjectsController.cs
@@ -4,149 +4,148 @@ using blackbird_crm.Models;
 using blackbird_crm.Data;
 using blackbird_crm.Models.RequestModels.Projects;
 
-namespace blackbird_crm.Controllers
+namespace blackbird_crm.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProjectsController : ControllerBase
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProjectsController : ControllerBase
+    private readonly AppDbContext _context;
+
+    public ProjectsController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
+        _context = context;
+    }
 
-        public ProjectsController(AppDbContext context)
+    [HttpGet]
+    public async Task<IActionResult> GetProjects([FromQuery] int? clientId = null, [FromQuery] string? searchQuery = null)
+    {
+        var projectsQuery = _context.Projects
+            .Include(p => p.Client)
+            .Select(p => new
+            {
+                p.Id,
+                p.ProjectName,
+                StartDate = p.StartDate.ToString("yyyy-MM-dd"),
+                p.Status,
+                ClientName = p.Client.ClientName,
+                p.ClientId
+            }).AsQueryable();
+
+        if (!string.IsNullOrEmpty(searchQuery))
         {
-            _context = context;
+            searchQuery = searchQuery.ToLower();
+            projectsQuery = projectsQuery.Where(p =>
+                p.ProjectName.ToLower().Contains(searchQuery) ||
+                p.ClientName.ToLower().Contains(searchQuery)
+            );
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetProjects([FromQuery] string? searchQuery = null, [FromQuery] string? selectedClientId = null)
+        if (clientId is not null)
         {
-            var projectsQuery = _context.Projects
-                .Include(p => p.Client)
-                .Select(p => new
-                {
-                    p.Id,
-                    p.ProjectName,
-                    StartDate = p.StartDate.ToString("yyyy-MM-dd"),
-                    p.Status,
-                    ClientName = p.Client.ClientName,
-                    p.ClientId
-                }).AsQueryable();
-
-            if (!string.IsNullOrEmpty(searchQuery))
-            {
-                searchQuery = searchQuery.ToLower();
-                projectsQuery = projectsQuery.Where(p =>
-                    p.ProjectName.ToLower().Contains(searchQuery) ||
-                    p.ClientName.ToLower().Contains(searchQuery)
-                );
-            }
-
-            if (!string.IsNullOrEmpty(selectedClientId) && int.TryParse(selectedClientId, out int clientId))
-            {
-                projectsQuery = projectsQuery.Where(p => p.ClientId == clientId);
-            }
-
-            var projects = await projectsQuery.ToListAsync();
-
-            return Ok(projects);
+            projectsQuery = projectsQuery.Where(p => p.ClientId == clientId);
         }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetProject(int id)
+        var projects = await projectsQuery.ToListAsync();
+
+        return Ok(projects);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProject(int id)
+    {
+        var project = await _context.Projects
+            .Include(p => p.Client)
+            .FirstOrDefaultAsync(p => p.Id == id);
+        if (project == null)
         {
-            var project = await _context.Projects
-                .Include(p => p.Client)
-                .FirstOrDefaultAsync(p => p.Id == id);
-            if (project == null)
-            {
-                return NotFound();
-            }
-            return Ok(project);
+            return NotFound();
+        }
+        return Ok(project);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProject([FromBody] CreateProjectRequest createProjectRequest)
+    {
+        if (createProjectRequest == null)
+        {
+            return BadRequest();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateProject([FromBody] CreateProjectRequest createProjectRequest)
+        var client = await _context.Clients.FindAsync(createProjectRequest.ClientId);
+        if (client == null)
         {
-            if (createProjectRequest == null)
-            {
-                return BadRequest();
-            }
-
-            var client = await _context.Clients.FindAsync(createProjectRequest.ClientId);
-            if (client == null)
-            {
-                return BadRequest("Invalid client ID.");
-            }
-
-            var project = new Project {
-                Client = client,
-                ProjectName = createProjectRequest.ProjectName,
-                StartDate = DateTime.UtcNow,
-                Status = createProjectRequest.Status
-            };
-
-            await _context.Projects.AddAsync(project);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction(nameof(GetProject), new { id = project.Id }, project);
+            return BadRequest("Invalid client ID.");
         }
 
+        var project = new Project {
+            Client = client,
+            ProjectName = createProjectRequest.ProjectName,
+            StartDate = DateTime.UtcNow,
+            Status = createProjectRequest.Status
+        };
 
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateProject(int id, [FromBody] EditProjectRequest editProjectRequest)
+        await _context.Projects.AddAsync(project);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetProject), new { id = project.Id }, project);
+    }
+
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProject(int id, [FromBody] EditProjectRequest editProjectRequest)
+    {
+        if (id <= 0 || editProjectRequest == null)
         {
-            if (id <= 0 || editProjectRequest == null)
-            {
-                return BadRequest("Invalid request.");
-            }
-
-            var existingProject = await _context.Projects
-                .Include(p => p.Client)
-                .FirstOrDefaultAsync(p => p.Id == id);
-
-            if (existingProject == null)
-            {
-                return NotFound();
-            }
-
-            var client = await _context.Clients.FindAsync(editProjectRequest.ClientId);
-            if (client == null)
-            {
-                return BadRequest("Invalid client ID.");
-            }
-
-            existingProject.ProjectName = editProjectRequest.ProjectName;
-            existingProject.Status = editProjectRequest.Status;
-            existingProject.Client = client;
-
-
-            _context.Projects.Update(existingProject);
-            await _context.SaveChangesAsync();
-
-            return Ok(new
-            {
-                existingProject.Id,
-                existingProject.ProjectName,
-                existingProject.StartDate,
-                existingProject.Status,
-                ClientName = existingProject.Client.ClientName,
-                existingProject.ClientId
-            });
+            return BadRequest("Invalid request.");
         }
 
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteProject(int id)
+        var existingProject = await _context.Projects
+            .Include(p => p.Client)
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+        if (existingProject == null)
         {
-            var project = await _context.Projects.FindAsync(id);
-            if (project == null)
-            {
-                return NotFound();
-            }
-
-            _context.Projects.Remove(project);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
+            return NotFound();
         }
+
+        var client = await _context.Clients.FindAsync(editProjectRequest.ClientId);
+        if (client == null)
+        {
+            return BadRequest("Invalid client ID.");
+        }
+
+        existingProject.ProjectName = editProjectRequest.ProjectName;
+        existingProject.Status = editProjectRequest.Status;
+        existingProject.Client = client;
+
+
+        _context.Projects.Update(existingProject);
+        await _context.SaveChangesAsync();
+
+        return Ok(new
+        {
+            existingProject.Id,
+            existingProject.ProjectName,
+            existingProject.StartDate,
+            existingProject.Status,
+            ClientName = existingProject.Client.ClientName,
+            existingProject.ClientId
+        });
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProject(int id)
+    {
+        var project = await _context.Projects.FindAsync(id);
+        if (project == null)
+        {
+            return NotFound();
+        }
+
+        _context.Projects.Remove(project);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
     }
 }
